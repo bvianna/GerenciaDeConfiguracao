c75af888b5f8aa8538efb6cfb6085e2e19fb9e5b
Bruno Vianna de Oliveira <bviannaoliveira@gmail.com>
2017-03-11 19:23:34 -0300

    versao 1.0

---
+++ EngenhariaDeSoftwareII/src/com/es/br/ClasseA.java
@@ -0,0 +1,30 @@
+package com.es.br;
+
+public class ClasseA {
+
+	private int A1;
+	private float A2;
+
+	public ClasseA(int a1, float a2) {
+		super();
+		A1 = a1;
+		A2 = a2;
+	}
+
+	private int getA1() {
+		return A1;
+	}
+	
+	private void setA1(int a1) {
+		A1 = a1;
+	}
+	
+	private float getA2() {
+		return A2;
+	}
+	
+	private void setA2(float a2) {
+		A2 = a2;
+	}
+		
+}
