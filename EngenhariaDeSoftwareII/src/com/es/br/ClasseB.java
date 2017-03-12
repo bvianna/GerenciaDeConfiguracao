edf93b9854566f836b523b9e88e694de23282faa
Bruno Vianna de Oliveira <bviannaoliveira@gmail.com>
2017-03-12 12:51:38 -0300

    Versao 1.3

--- EngenhariaDeSoftwareII/src/com/es/br/ClasseB.java
+++ EngenhariaDeSoftwareII/src/com/es/br/ClasseB.java
@@ -1,10 +1,9 @@
 package com.es.br;
 
 public class ClasseB {
-	
+
 	private int A1;
 	private float A2;
-
 
 	public ClasseB(int a1, float a2) {
 		super();
@@ -12,26 +11,21 @@ public class ClasseB {
 		A2 = a2;
 	}
 
-
 	private int getA1() {
 		return A1;
 	}
-
 
 	private void setA1(int a1) {
 		A1 = a1;
 	}
 
-
 	private float getA2() {
 		return A2;
 	}
-
 
 	private void setA2(float a2) {
 		A2 = a2;
 	}
 	
 	
-
 }
